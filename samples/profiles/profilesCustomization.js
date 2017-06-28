66d1ba44b0637cb0ac066ef6d0050e7ea97c85e7
MartinDonnelly <martin.w.donnelly@gmail.com>
2017-06-28 16:27:23 +0100

    Add include files

---
+++ samples/profiles/profilesCustomization.js
@@ -0,0 +1,13 @@
+////
+// @author Tony McGuckin, IBM
+// @name Profiles Customization
+// @version 0.1
+// @date February, 2017
+//
+if(typeof(dojo) != "undefined") {
+    dojo.place(
+        "<link rel=\"stylesheet\" type=\"text/css\" href=\"/files/muse-static/samples/profiles/profilesCustomization.css\"></link>",
+        dojo.doc.head,
+        "last"
+    );
+}
