cb9955ee29c5aa411d814c5f9f0ea9551d67b87f
Brian Gleeson <brian.gleeson@ie.ibm.com>
2017-09-27 12:50:31 +0100

    Clean up code of Customizer samples

--- samples/newRiver/newsRiverSectionedHashChange.user.js
+++ samples/newRiver/newsRiverSectionedHashChange.user.js
@@ -2,12 +2,12 @@
 // @copyright    Copyright IBM Corp. 2017
 // @name         newsRiverSectionedColour
 // @version      0.10
-// @description  *** PROTOTYPE CODE *** displays river of news items as separate card styled sections 
+// @description  *** PROTOTYPE CODE *** displays river of news items as separate card styled sections
 //                                      and changes the background colour based on has event
 //
 // @namespace  http://ibm.com
 //
-// @author       Tony Estrada - Padraic Edwards
+// @author       Tony Estrada - Padraic Edwards - Brian Gleeson
 //
 // @include      *://*connections*.ibm.com/homepage/*
 // @include      *://apps.*.collabserv.com/homepage/*
@@ -29,72 +29,53 @@
 //
 // ==/UserScript==
 
-if(typeof(dojo) != "undefined") {
-	
-   require(["dojo/domReady!"], function(){
-         //---------------------------------------------
-         // HELPER FUNCTIONS
-         var waitFor = function(callback, elXpath, maxInter, waitTime) {
-            if(!maxInter) var maxInter = 100;  // number of intervals before expiring
-            if(!waitTime) var waitTime = 10;  // 1000=1 second
-            if(!elXpath) return;
-            
-            var waitInter = 0;  // current interval
-            var intId = setInterval( function(){
-               if (++waitInter<maxInter && !dojo.query(elXpath).length) return;
-               clearInterval(intId);
-               callback();
-            }, waitTime);
-         };
- 		
-		function triggerHashChangeEvent(){
-			//get the current hashValue
-			var hashValue = window.location.hash;		
-			if(hashValue == "#myStream/imFollowing/all"){
-				changeNewsRiverColour(240, 158, 231);
-			}
-			if(hashValue == "#atMentions/atMentions"){
-				changeNewsRiverColour(110, 158, 122);
-			}
-			if(hashValue == "#myNotifications/forme/all"){
-				changeNewsRiverColour(90, 111, 134);
-			}
-			if(hashValue == "#actionRequired/actionRequired/all"){
-				changeNewsRiverColour(20, 101, 234);
-			}
-			if(hashValue == "#saved/saved/all"){
-				changeNewsRiverColour(230, 2, 155);
-			}
-		}
-		
-		function changeNewsRiverColour(rgb1,rgb2,rgb3){
-			activity stream cards colour
-			dojo.place(
-			"<style>"+
-            "#activityStream ul.lotusStream { background-color:rgba("+rgb1+","+rgb2+","+rgb3+", "+(location.href.indexOf("/profiles/")>0?"0":"1")+"); border:0px; padding:5px 0px 5px 0px; }"+
-            "#activityStream ul.lotusStream > li { background-color:white; margin:25px 15px 25px 3px; box-shadow:5px 5px 15px #aaaaaa; border-radius:5px; }"+
-            "#activityStream ul.lotusStream > li:after { height:0px; }"+
-            ".lotusui30 .lotusBoard .lotusStream .lotusPostHover, .lotusui30 .lotusBoard .lotusStream .lotusPostSelected { border-radius: inherit; /* -webkit-transform: translate(3px,3px); -webkit-transition: width 2s, height 2s, -webkit-transform 0.25s;*/ }"+
-            ".lotusui30 .lotusStream .filterArea { border-radius:0 0 5px 5px; border-width:0 1px 1px }"+
-            ".lotusui30 .lotusStream .filterAreaInner { border: 0px }"+
-			"</style>",
-			dojo.body(),"append");		
-		}
-		
-		//listen for onHashChange event
-        window.onhashchange = triggerHashChangeEvent;	
-		
-		//set initial background colour of news river
-		dojo.place(
-        "<style>"+
-           "#activityStream ul.lotusStream { background-color:rgba(240, 240, 240, "+(location.href.indexOf("/profiles/")>0?"0":"1")+"); border:0px; padding:5px 0px 5px 0px; }"+
-            "#activityStream ul.lotusStream > li { background-color:white; margin:25px 15px 25px 3px; box-shadow:5px 5px 15px #aaaaaa; border-radius:5px; }"+
-            "#activityStream ul.lotusStream > li:after { height:0px; }"+
-            ".lotusui30 .lotusBoard .lotusStream .lotusPostHover, .lotusui30 .lotusBoard .lotusStream .lotusPostSelected { border-radius: inherit; /* -webkit-transform: translate(3px,3px); -webkit-transition: width 2s, height 2s, -webkit-transform 0.25s;*/ }"+
-            ".lotusui30 .lotusStream .filterArea { border-radius:0 0 5px 5px; border-width:0 1px 1px }"+
-            ".lotusui30 .lotusStream .filterAreaInner { border: 0px }"+
+if (typeof(dojo) != "undefined") {
+   require(["dojo/domReady!"], function() {
+
+    function handleHashChangeEvent() {
+      // Get the current hashValue
+      var hashValue = window.location.hash;
+
+      if (hashValue == "#myStream/imFollowing/all") {
+        changeNewsRiverColour(240, 158, 231);
+      }
+      if (hashValue == "#atMentions/atMentions") {
+        changeNewsRiverColour(110, 158, 122);
+      }
+      if (hashValue == "#myNotifications/forme/all") {
+        changeNewsRiverColour(90, 111, 134);
+      }
+      if (hashValue == "#actionRequired/actionRequired/all") {
+        changeNewsRiverColour(20, 101, 234);
+      }
+      if (hashValue == "#saved/saved/all"){
+        changeNewsRiverColour(230, 2, 155);
+      }
+    }
+
+    function changeNewsRiverColour(rgb1, rgb2, rgb3) {
+      // Remove old style
+      var newsRiverStyle = dojo.byId("newsRiverStyle");
+      if (newsRiverStyle) {
+        dojo.destroy(newsRiverStyle); // destroy current style
+      }
+
+      // Add new style for activity stream cards colour
+      dojo.place("<style id='newsRiverStyle'>"+
+          "#activityStream ul.lotusStream { background-color:rgba(" + rgb1 + "," + rgb2 + "," + rgb3 + ", " + (location.href.indexOf("/profiles/") > 0 ? "0" : "1") + "); border:0px; padding:5px 0px 5px 0px; }" +
+          "#activityStream ul.lotusStream > li { background-color:white; margin:25px 15px 25px 3px; box-shadow:5px 5px 15px #aaaaaa; border-radius:5px; }" +
+          "#activityStream ul.lotusStream > li:after { height:0px; }" +
+          ".lotusui30 .lotusBoard .lotusStream .lotusPostHover, .lotusui30 .lotusBoard .lotusStream .lotusPostSelected { border-radius: inherit; /* -webkit-transform: translate(3px,3px); -webkit-transition: width 2s, height 2s, -webkit-transform 0.25s;*/ }" +
+          ".lotusui30 .lotusStream .filterArea { border-radius:0 0 5px 5px; border-width:0 1px 1px }" +
+          ".lotusui30 .lotusStream .filterAreaInner { border: 0px }" +
         "</style>",
-        dojo.body(),"");		
+        dojo.body(),"append");
+    }
+
+    //listen for onHashChange event
+    window.onhashchange = handleHashChangeEvent;
+
+    //set initial background colour of news river
+    handleHashChangeEvent();
    });
-   
 }
