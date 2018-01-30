1c1cf8d55655db1b202e1b4deded4b202c6550a3
Padraic Edwards <padraic.edwards@ie.ibm.com>
2018-01-30 20:55:36 +0000

    Add repoName property

--- samples/profiles/profilesCustomization.js
+++ samples/profiles/profilesCustomization.js
@@ -6,7 +6,7 @@
 //
 if(typeof(dojo) != "undefined") {
     dojo.place(
-        "<link rel=\"stylesheet\" type=\"text/css\" href=\"/files/muse-static/samples/profiles/profilesCustomization.css\"></link>",
+        "<link rel=\"stylesheet\" type=\"text/css\" href=\"/files/muse-static/samples/profiles/profilesCustomization.css?repoName=global-samples\"></link>",
         dojo.doc.head,
         "last"
     );
