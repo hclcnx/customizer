265a00e65ba3d254de5f353f2556e52836d04013
Jarkko Pietikäinen <jarkko.pietikainen@netwell.fi>
2018-09-19 16:09:03 +0300

    Update helloWorld.user.js

--- samples/helloWorld/helloWorld.user.js
+++ samples/helloWorld/helloWorld.user.js
@@ -45,7 +45,7 @@ if(typeof(dojo) != "undefined") {
             waitFor( function(){
 			// wait until the "loading..." node has been hidden
 			// indicating that we have loaded content.
-   			dojo.query("span.shareSome-title")[0].textContent="Hello World! ";
+   			dojo.query("span.shareSome-title")[0].textContent="Hello CNX! ";
        	          },
 		  ".lotusStreamTopLoading div.loaderMain.lotusHidden");
       } catch(e) {
