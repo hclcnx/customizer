0566739d02d5cc855f1dac24f7a995378c288589
Jarkko Pietikäinen <jarkko.pietikainen@netwell.fi>
2018-09-19 18:08:07 +0300

    moving sample files to static dir

---
+++ static/profiles/profilesCustomization.js
@@ -0,0 +1,13 @@
+////
+// @author Tony McGuckin, IBM
+// @name Profiles Customization
+// @version 0.1
+// @date February, 2017
+//
+if(typeof(dojo) != "undefined") {
+    dojo.place(
+        "<link rel=\"stylesheet\" type=\"text/css\" href=\"/files/customizer/profiles/profilesCustomization.css?repoName=customizer\"></link>",
+        dojo.doc.head,
+        "last"
+    );
+}
